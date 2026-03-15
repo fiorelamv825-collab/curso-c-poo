1158276dc9a2828b43446d7fee1b7d6dd137c9c9
Fiorela Mamani <fiorelamv825@gmail.com>
2026-03-15 15:49:32 -0500

    condicional if y else

---
+++ adicional condicionales 1 .cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int nota;
+    cout <<"ingrese su nota "<<endl;
+    cin >> nota ;
+
+    if (nota >=11)
+    {
+       cout<< " usted ha aprobado "<<endl;
+    }else
+    {
+        cout <<"usted ha desaprobado"<<endl;
+    } 
+  return 0;
+}
